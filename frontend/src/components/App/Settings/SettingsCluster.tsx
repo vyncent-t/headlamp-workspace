headlamp-k8s/headlamp

frontend: Improve the rename cluster logic and ui in cluster settings
Changes Include
SettingsCluster.tsx:
- handles error logic from backend to prevent users from entering a name/custom name already in use by another cluster
- introduces new confirm dialog for when users enter clashing names

ConfirmDialog.tsx:
- introduces optional params for disabling the Yes and No button, allowing for single option "continue" dialog

clusterApi.ts:
- reworks the current renameCluster function to now handle renaming for dynamic and non dynamic clusters differently

Signed-off-by: Vincent T <vtaylor@microsoft.com>

frontend/src/components/App/Settings/SettingsCluster.tsx
@@ -18,8 +18,12 @@ import helpers, { ClusterSettings } from '../../../helpers';
 import { useCluster, useClustersConf } from '../../../lib/k8s';
 import { deleteCluster, parseKubeConfig, renameCluster } from '../../../lib/k8s/apiProxy';
 import { setConfig, setStatelessConfig } from '../../../redux/configSlice';
-import { findKubeconfigByClusterName, updateStatelessClusterKubeconfig } from '../../../stateless/';
-import { Link, Loader, NameValueTable, SectionBox } from '../../common';
+import {
+  findKubeconfigByClusterName,
+  findKubeconfigByClusterPathID,
+  updateStatelessClusterKubeconfig,
+} from '../../../stateless/';
+import { ConfirmDialog, Link, Loader, NameValueTable, SectionBox } from '../../common';
 import ConfirmButton from '../../common/ConfirmButton';
 import Empty from '../../common/EmptyContent';
 
@@ -90,6 +94,8 @@ export default function SettingsCluster() {
   const [cluster, setCluster] = React.useState(useCluster() || '');
   const clusterFromURLRef = React.useRef('');
   const [newClusterName, setNewClusterName] = React.useState(cluster || '');
+  const [clusterErrorDialogOpen, setClusterErrorDialogOpen] = React.useState(false);
+
   const theme = useTheme();
 
   const history = useHistory();
@@ -97,18 +103,58 @@ export default function SettingsCluster() {
   const location = useLocation();
 
   const clusterInfo = (clusterConf && clusterConf[cluster || '']) || null;
-  const source = clusterInfo?.meta_data?.source || '';
+  const originalName = clusterInfo?.meta_data?.originalName;
+  const displayName = originalName || (clusterInfo ? clusterInfo.name : '');
+  const source = clusterInfo?.meta_data?.source;
+  // Note: display original name is currently only supported for non dynamic clusters from kubeconfig sources.
+  const pathID = clusterInfo?.meta_data?.pathID || '';
+
+  // This function is part of a double check, this is meant to check all the cluster names currently in use as display names
+  // Note: if the metadata is not available or does not load, another check is done in the backend to ensure the name is unique in its own config
+  function checkNameInUse(name: string) {
+    if (!clusterConf) {
+      return false;
+    }
+
+    // These are the display names of the clusters, renamed clusters have their display name as the custom name
+    const clusterNames = Object.values(clusterConf).map(cluster => cluster.name);
+
+    // We also include original names from non dynamic clusters if they exist
+    const originalNames = Object.values(clusterConf)
+      .map(cluster => cluster.meta_data?.originalName)
+      .filter(originalName => originalName !== undefined);
+
+    const allNames = [...clusterNames, ...originalNames];
+
+    const nameInUse = allNames.includes(name);
+
+    return nameInUse;
+  }
 
   const handleUpdateClusterName = (source: string) => {
+    const nameInUse = checkNameInUse(newClusterName);
+
+    // if the name is in use we prevent the user from updating the name
+    if (nameInUse) {
+      setClusterErrorDialogOpen(true);
+      return;
+    }
+
     try {
-      renameCluster(cluster || '', newClusterName, source)
+      renameCluster(cluster || '', newClusterName, source, pathID)
         .then(async config => {
           if (cluster) {
-            const kubeconfig = await findKubeconfigByClusterName(cluster);
+            const kubeconfig =
+              source === 'kubeconfig'
+                ? await findKubeconfigByClusterPathID(pathID)
+                : await findKubeconfigByClusterName(cluster);
             if (kubeconfig !== null) {
               await updateStatelessClusterKubeconfig(kubeconfig, newClusterName, cluster);
               // Make another request for updated kubeconfig
-              const updatedKubeconfig = await findKubeconfigByClusterName(cluster);
+              const updatedKubeconfig =
+                source === 'kubeconfig'
+                  ? await findKubeconfigByClusterPathID(pathID)
+                  : await findKubeconfigByClusterName(cluster);
               if (updatedKubeconfig !== null) {
                 parseKubeConfig({ kubeconfig: updatedKubeconfig })
                   .then((config: any) => {
@@ -127,6 +173,7 @@ export default function SettingsCluster() {
           window.location.reload();
         })
         .catch((err: Error) => {
+          setClusterErrorDialogOpen(true);
           console.error('Error updating cluster name:', err.message);
         });
     } catch (error) {
@@ -314,20 +361,38 @@ export default function SettingsCluster() {
     );
   }
 
+  function ClusterErrorDialog() {
+    return (
+      <ConfirmDialog
+        onConfirm={() => {
+          setClusterErrorDialogOpen(false);
+        }}
+        handleClose={() => {
+          setClusterErrorDialogOpen(false);
+        }}
+        disableNoButton
+        open={clusterErrorDialogOpen}
+        title={t('translation|Invalid custom name')}
+        description={t(
+          'translation|Custom names must be unique and follow valid format for cluster rename. '
+        )}
+        confirmLabel={t('translation|Okay')}
+      >
+        {t('translation|Close')}
+      </ConfirmDialog>
+    );
+  }
+
   // Display the original name of the cluster if it was loaded from a kubeconfig file.
   function ClusterName() {
-    const originalName = clusterInfo?.meta_data?.originalName;
-    const source = clusterInfo?.meta_data?.source;
-    // Note: display original name is currently only supported for non dynamic clusters from kubeconfig sources.
-    const displayOriginalName = source === 'kubeconfig' && originalName;
-
     return (
       <>
+        {clusterErrorDialogOpen && <ClusterErrorDialog />}
         <Typography>{t('translation|Name')}</Typography>
-        {displayOriginalName && (
+        {displayName && (
           <Typography variant="body2" color="textSecondary">
-            {t('translation|Original name: {{ originalName }}', {
-              originalName: originalName,
+            {t('translation|Original name: {{ displayName }}', {
+              displayName: displayName,
             })}
           </Typography>
         )}
@@ -387,7 +452,7 @@ export default function SettingsCluster() {
                             confirmTitle={t('translation|Change name')}
                             confirmDescription={t(
                               'translation|Are you sure you want to change the name for "{{ clusterName }}"?',
-                              { clusterName: cluster }
+                              { clusterName: displayName }
                             )}
                             disabled={!newClusterName || !isValidCurrentName}
                           >

frontend/src/stateless/index.ts
@@ -272,6 +272,72 @@ export function findKubeconfigByClusterName(clusterName: string): Promise<string
 }
 
 /**
+ * Finds a kubeconfig of a non-dynamic cluster by pathID.
+ * @param pathID - The pathID of the cluster.
+ * @returns A promise that resolves with the kubeconfig, or null if not found.
+ * @throws Error if IndexedDB is not supported.
+ * @throws Error if the kubeconfig is invalid.
+ *
+ * Note: This function is needed to find kubeconfigs for non-dynamic clusters using their unique pathID and should be used independently.
+ * Currently only non-dynamic clusters have a pathID.
+ *
+ * This function will be used to both rename and delete non dynamic clusters.
+ */
+export function findKubeconfigByClusterPathID(pathID: string): Promise<string | null> {
+  return new Promise<string | null>(async (resolve, reject) => {
+    try {
+      const request = indexedDB.open('kubeconfigs', 1) as any;
+
+      // The onupgradeneeded event is fired when the database is created for the first time.
+      request.onupgradeneeded = handleDatabaseUpgrade;
+
+      // The onsuccess event is fired when the database is opened.
+      // This event is where you specify the actions to take when the database is opened.
+      request.onsuccess = function handleDatabaseSuccess(event: DatabaseEvent) {
+        const db = event.target.result;
+        const transaction = db.transaction(['kubeconfigStore'], 'readonly');
+        const store = transaction.objectStore('kubeconfigStore');
+
+        // The onsuccess event is fired when the request has succeeded.
+        // This is where you handle the results of the request.
+        // The result is the cursor. It is used to iterate through the object store.
+        // The cursor is null when there are no more objects to iterate through.
+        // The cursor is used to find the kubeconfig by cluster name.
+        store.openCursor().onsuccess = function storeSuccess(event: Event) {
+          const successEvent = event as CursorSuccessEvent;
+          const cursor = successEvent.target.result;
+          if (cursor) {
+            const kubeconfigObject = cursor.value;
+            const kubeconfig = kubeconfigObject.kubeconfig;
+
+            const parsedKubeconfig = jsyaml.load(atob(kubeconfig)) as KubeconfigObject;
+
+            // Find the context with the matching pathID
+            const matchingContext = parsedKubeconfig.contexts.find(
+              context => context.context.pathID === pathID
+            );
+
+            if (matchingContext) {
+              resolve(kubeconfig);
+            } else {
+              cursor.continue();
+            }
+          } else {
+            resolve(null); // No matching kubeconfig found
+          }
+        };
+      };
+
+      // The onerror event is fired when the database is opened.
+      // This is where you handle errors.
+      request.onerror = handleDataBaseError;
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+/**
  * In the backend we use a unique ID to identify a user. If there is no ID in localStorage
  * we generate a new one and store it in localStorage. We then combine it with the
  * cluster name and this headlamp-userId to create a unique ID for a cluster. If we don't

vyncent-t/headlamp-workspace

frontend/src/components/App/Settings/SettingsCluster.tsx
@@ -19,8 +19,12 @@ import helpers, { ClusterSettings } from '../../../helpers';
 import { useCluster, useClustersConf } from '../../../lib/k8s';
 import { deleteCluster, parseKubeConfig, renameCluster } from '../../../lib/k8s/apiProxy';
 import { setConfig, setStatelessConfig } from '../../../redux/configSlice';
-import { findKubeconfigByClusterName, updateStatelessClusterKubeconfig } from '../../../stateless/';
-import { Link, Loader, NameValueTable, SectionBox } from '../../common';
+import {
+  findKubeconfigByClusterID,
+  findKubeconfigByClusterName,
+  updateStatelessClusterKubeconfig,
+} from '../../../stateless/';
+import { ConfirmDialog, Link, Loader, NameValueTable, SectionBox } from '../../common';
 import ConfirmButton from '../../common/ConfirmButton';
 import Empty from '../../common/EmptyContent';
 
@@ -92,6 +96,8 @@ export default function SettingsCluster() {
   const [cluster, setCluster] = React.useState(useCluster() || '');
   const clusterFromURLRef = React.useRef('');
   const [newClusterName, setNewClusterName] = React.useState(cluster || '');
+  const [clusterErrorDialogOpen, setClusterErrorDialogOpen] = React.useState(false);
+
   const theme = useTheme();
 
   const history = useHistory();
@@ -99,18 +105,58 @@ export default function SettingsCluster() {
   const location = useLocation();
 
   const clusterInfo = (clusterConf && clusterConf[cluster || '']) || null;
-  const source = clusterInfo?.meta_data?.source || '';
+  const originalName = clusterInfo?.meta_data?.originalName;
+  const displayName = originalName || (clusterInfo ? clusterInfo.name : '');
+  const source = clusterInfo?.meta_data?.source;
+  // Note: display original name is currently only supported for non dynamic clusters from kubeconfig sources.
+  const clusterID = clusterInfo?.meta_data?.clusterID || '';
+
+  // This function is part of a double check, this is meant to check all the cluster names currently in use as display names
+  // Note: if the metadata is not available or does not load, another check is done in the backend to ensure the name is unique in its own config
+  function checkNameInUse(name: string) {
+    if (!clusterConf) {
+      return false;
+    }
+
+    // These are the display names of the clusters, renamed clusters have their display name as the custom name
+    const clusterNames = Object.values(clusterConf).map(cluster => cluster.name);
+
+    // We also include original names from non dynamic clusters if they exist
+    const originalNames = Object.values(clusterConf)
+      .map(cluster => cluster.meta_data?.originalName)
+      .filter(originalName => originalName !== undefined);
+
+    const allNames = [...clusterNames, ...originalNames];
+
+    const nameInUse = allNames.includes(name);
+
+    return nameInUse;
+  }
 
   const handleUpdateClusterName = (source: string) => {
+    const nameInUse = checkNameInUse(newClusterName);
+
+    // if the name is in use we prevent the user from updating the name
+    if (nameInUse) {
+      setClusterErrorDialogOpen(true);
+      return;
+    }
+
     try {
-      renameCluster(cluster || '', newClusterName, source)
+      renameCluster(cluster || '', newClusterName, source, clusterID)
         .then(async config => {
           if (cluster) {
-            const kubeconfig = await findKubeconfigByClusterName(cluster);
+            const kubeconfig =
+              source === 'kubeconfig'
+                ? await findKubeconfigByClusterID(clusterID)
+                : await findKubeconfigByClusterName(cluster);
             if (kubeconfig !== null) {
               await updateStatelessClusterKubeconfig(kubeconfig, newClusterName, cluster);
               // Make another request for updated kubeconfig
-              const updatedKubeconfig = await findKubeconfigByClusterName(cluster);
+              const updatedKubeconfig =
+                source === 'kubeconfig'
+                  ? await findKubeconfigByClusterID(clusterID)
+                  : await findKubeconfigByClusterName(cluster);
               if (updatedKubeconfig !== null) {
                 parseKubeConfig({ kubeconfig: updatedKubeconfig })
                   .then((config: any) => {
@@ -129,6 +175,7 @@ export default function SettingsCluster() {
           window.location.reload();
         })
         .catch((err: Error) => {
+          setClusterErrorDialogOpen(true);
           console.error('Error updating cluster name:', err.message);
         });
     } catch (error) {
@@ -316,20 +363,38 @@ export default function SettingsCluster() {
     );
   }
 
+  function ClusterErrorDialog() {
+    return (
+      <ConfirmDialog
+        onConfirm={() => {
+          setClusterErrorDialogOpen(false);
+        }}
+        handleClose={() => {
+          setClusterErrorDialogOpen(false);
+        }}
+        disableNoButton
+        open={clusterErrorDialogOpen}
+        title={t('translation|Invalid custom name')}
+        description={t(
+          'translation|Custom names must be unique and follow valid format for cluster rename. '
+        )}
+        confirmLabel={t('translation|Okay')}
+      >
+        {t('translation|Close')}
+      </ConfirmDialog>
+    );
+  }
+
   // Display the original name of the cluster if it was loaded from a kubeconfig file.
   function ClusterName() {
-    const originalName = clusterInfo?.meta_data?.originalName;
-    const source = clusterInfo?.meta_data?.source;
-    // Note: display original name is currently only supported for non dynamic clusters from kubeconfig sources.
-    const displayOriginalName = source === 'kubeconfig' && originalName;
-
     return (
       <>
+        {clusterErrorDialogOpen && <ClusterErrorDialog />}
         <Typography>{t('translation|Name')}</Typography>
-        {displayOriginalName && (
+        {displayName && (
           <Typography variant="body2" color="textSecondary">
-            {t('translation|Original name: {{ originalName }}', {
-              originalName: originalName,
+            {t('translation|Original name: {{ displayName }}', {
+              displayName: displayName,
             })}
           </Typography>
         )}
@@ -389,7 +454,7 @@ export default function SettingsCluster() {
                             confirmTitle={t('translation|Change name')}
                             confirmDescription={t(
                               'translation|Are you sure you want to change the name for "{{ clusterName }}"?',
-                              { clusterName: cluster }
+                              { clusterName: displayName }
                             )}
                             disabled={!newClusterName || !isValidCurrentName}
                           >

frontend/src/stateless/index.ts
@@ -272,6 +272,72 @@ export function findKubeconfigByClusterName(clusterName: string): Promise<string
 }
 
 /**
+ * Finds a kubeconfig of a non-dynamic cluster by clusterID.
+ * @param clusterID - The clusterID of the cluster.
+ * @returns A promise that resolves with the kubeconfig, or null if not found.
+ * @throws Error if IndexedDB is not supported.
+ * @throws Error if the kubeconfig is invalid.
+ *
+ * Note: This function is needed to find kubeconfigs for non-dynamic clusters using their unique clusterID and should be used independently.
+ * Currently only non-dynamic clusters have a clusterID.
+ *
+ * This function will be used to both rename and delete non dynamic clusters.
+ */
+export function findKubeconfigByClusterID(clusterID: string): Promise<string | null> {
+  return new Promise<string | null>(async (resolve, reject) => {
+    try {
+      const request = indexedDB.open('kubeconfigs', 1) as any;
+
+      // The onupgradeneeded event is fired when the database is created for the first time.
+      request.onupgradeneeded = handleDatabaseUpgrade;
+
+      // The onsuccess event is fired when the database is opened.
+      // This event is where you specify the actions to take when the database is opened.
+      request.onsuccess = function handleDatabaseSuccess(event: DatabaseEvent) {
+        const db = event.target.result;
+        const transaction = db.transaction(['kubeconfigStore'], 'readonly');
+        const store = transaction.objectStore('kubeconfigStore');
+
+        // The onsuccess event is fired when the request has succeeded.
+        // This is where you handle the results of the request.
+        // The result is the cursor. It is used to iterate through the object store.
+        // The cursor is null when there are no more objects to iterate through.
+        // The cursor is used to find the kubeconfig by cluster name.
+        store.openCursor().onsuccess = function storeSuccess(event: Event) {
+          const successEvent = event as CursorSuccessEvent;
+          const cursor = successEvent.target.result;
+          if (cursor) {
+            const kubeconfigObject = cursor.value;
+            const kubeconfig = kubeconfigObject.kubeconfig;
+
+            const parsedKubeconfig = jsyaml.load(atob(kubeconfig)) as KubeconfigObject;
+
+            // Find the context with the matching clusterID
+            const matchingContext = parsedKubeconfig.contexts.find(
+              context => context.context.clusterID === clusterID
+            );
+
+            if (matchingContext) {
+              resolve(kubeconfig);
+            } else {
+              cursor.continue();
+            }
+          } else {
+            resolve(null); // No matching kubeconfig found
+          }
+        };
+      };
+
+      // The onerror event is fired when the database is opened.
+      // This is where you handle errors.
+      request.onerror = handleDataBaseError;
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+/**
  * In the backend we use a unique ID to identify a user. If there is no ID in localStorage
  * we generate a new one and store it in localStorage. We then combine it with the
  * cluster name and this headlamp-userId to create a unique ID for a cluster. If we don't