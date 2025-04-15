headlamp-k8s/headlamp

backend: Rework logic for renameCluster to prevent duplicate names and updates test
Signed-off-by: Vincent T <vtaylor@microsoft.com>

backend/cmd/headlamp.go
@@ -1508,9 +1508,15 @@ func (c *HeadlampConfig) deleteCluster(w http.ResponseWriter, r *http.Request) {
 	c.getConfig(w, r)
 }
 
-// Get path of kubeconfig from source.
+// Gets the path of the kubeconfig we load headlamp with from source.
+// Note for non dynamic clusters:
+//   - This will always give the kubeconfig we loaded headlamp with, but if more than one,
+//     it's not clear this will yield the correct value.
 func (c *HeadlampConfig) getKubeConfigPath(source string) (string, error) {
-	if source == "kubeconfig" {
+	// note: we need to have it in a const for the linting to pass
+	const sourceKubeconfig = "kubeconfig"
+
+	if source == sourceKubeconfig {
 		return c.kubeConfigPath, nil
 	}
 
@@ -1598,32 +1604,65 @@ func (c *HeadlampConfig) updateCustomContextToCache(config *api.Config, clusterN
 }
 
 // getPathAndLoadKubeconfig gets the path of the kubeconfig file and loads it.
-func (c *HeadlampConfig) getPathAndLoadKubeconfig(source, clusterName string) (string, *api.Config, error) {
-	// Get path of kubeconfig from source
-	path, err := c.getKubeConfigPath(source)
-	if err != nil {
-		logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
-			err, "getting kubeconfig file")
+// Note: the loadpath param is the direct path of the original kubeconfig if the cluster source is "kubeconfig".
+func (c *HeadlampConfig) getPathAndLoadKubeconfig(
+	source,
+	clusterName string,
+	loadPath string,
+) (string, *api.Config, error) {
+	var usePath string
 
-		return "", nil, err
+	var kubeConfig *api.Config
+
+	const sourceKubeconfig = "kubeconfig"
+
+	if source == sourceKubeconfig {
+		// If working from non dynamic cluster, we need to load the direct path of the original kubeconfig,
+		// not the headlamp default loaded kubeconfig.
+		// This handles uses where we have multiple kubeconfigs that we wish to use in headlamp
+		config, err := clientcmd.LoadFromFile(loadPath)
+		if err != nil {
+			logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+				err, "loading kubeconfig file")
+
+			return "", nil, err
+		}
+
+		usePath = loadPath
+		kubeConfig = config
+	} else {
+		// else we are working with a non dynamic cluster
+		// and the rename process is handled with the kubeconfig file loaded by headlamp
+		path, err := c.getKubeConfigPath(source)
+		if err != nil {
+			logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+				err, "getting kubeconfig file")
+
+			return "", nil, err
+		}
+
+		config, err := clientcmd.LoadFromFile(path)
+		if err != nil {
+			logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+				err, "loading kubeconfig file")
+
+			return "", nil, err
+		}
+
+		usePath = path
+		kubeConfig = config
 	}
 
-	// Load kubeconfig file
-	config, err := clientcmd.LoadFromFile(path)
-	if err != nil {
-		logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
-			err, "loading kubeconfig file")
-
-		return "", nil, err
-	}
-
-	return path, config, nil
+	return usePath, kubeConfig, nil
 }
 
 // Handler for renaming a cluster.
 func (c *HeadlampConfig) renameCluster(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	clusterName := vars["name"]
+	parts := strings.SplitN(r.URL.Query().Get("clusterPathID"), ":", 2)
+	configPath := parts[0]
+
 	// Parse request body.
 	var reqBody RenameClusterRequest
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
@@ -1641,34 +1680,28 @@ func (c *HeadlampConfig) renameCluster(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var loadPath string
+	if reqBody.Source == "kubeconfig" {
+		loadPath = configPath
+	}
+
 	// Get path of kubeconfig from source
-	path, config, err := c.getPathAndLoadKubeconfig(reqBody.Source, clusterName)
+	path, config, err := c.getPathAndLoadKubeconfig(reqBody.Source, clusterName, loadPath)
 	if err != nil {
 		http.Error(w, "getting kubeconfig file", http.StatusInternalServerError)
 		return
 	}
 
-	// Find the context with the given cluster name
-	contextName := clusterName
+	isUnique := checkUniqueName(config, clusterName, reqBody.NewClusterName)
+	if !isUnique {
+		http.Error(w, "custom name already in use", http.StatusBadRequest)
+		logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+			err, "cluster name already exists in the kubeconfig")
 
-	// Iterate over the contexts to find the context with the given cluster name
-	for k, v := range config.Contexts {
-		info := v.Extensions["headlamp_info"]
-		if info != nil {
-			customObj, err := MarshalCustomObject(info, contextName)
-			if err != nil {
-				logger.Log(logger.LevelError, map[string]string{"cluster": contextName},
-					err, "marshaling custom object")
-
-				return
-			}
-
-			// Check if the CustomName field matches the cluster name
-			if customObj.CustomName != "" && customObj.CustomName == clusterName {
-				contextName = k
-			}
-		}
+		return
 	}
+
+	contextName := findMatchingContextName(config, clusterName)
 
 	if err := customNameToExtenstions(config, contextName, reqBody.NewClusterName, path); err != nil {
 		http.Error(w, "writing custom extension to kubeconfig", http.StatusInternalServerError)
@@ -1682,6 +1715,76 @@ func (c *HeadlampConfig) renameCluster(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	c.getConfig(w, r)
+}
+
+// findMatchingContextName checks all contexts, returning the key for whichever
+// has a matching customObj.CustomName, if any.
+func findMatchingContextName(config *api.Config, clusterName string) string {
+	contextName := clusterName
+
+	for k, v := range config.Contexts {
+		info := v.Extensions["headlamp_info"]
+		if info != nil {
+			customObj, err := MarshalCustomObject(info, contextName)
+			if err != nil {
+				logger.Log(logger.LevelError, map[string]string{"cluster": contextName},
+					err, "marshaling custom object")
+				continue
+			}
+
+			if customObj.CustomName == clusterName && customObj.CustomName != "" {
+				contextName = k
+			}
+		}
+	}
+
+	return contextName
+}
+
+// checkUniqueName returns false if 'newName' is already in 'names' otherwise returns true.
+// used for checking cluster names.
+func checkUniqueName(config *api.Config, contextName string, newName string) bool {
+	contextNames := make([]string, 0, len(config.Contexts))
+
+	for name := range config.Contexts {
+		contextNames = append(contextNames, name)
+		logger.Log(logger.LevelInfo, map[string]string{"cluster added": name},
+			nil, "context name")
+	}
+
+	// Iterate over the contexts and add the custom names
+	for _, y := range config.Contexts {
+		info := y.Extensions["headlamp_info"]
+		if info != nil {
+			customObj, err := MarshalCustomObject(info, contextName)
+			if err != nil {
+				logger.Log(logger.LevelError, map[string]string{"cluster": contextName},
+					err, "marshaling custom object")
+			}
+
+			// add custom name if it is not empty
+			if customObj.CustomName != "" {
+				contextNames = append(contextNames, customObj.CustomName)
+			}
+		}
+	}
+
+	for _, current := range contextNames {
+		if current == newName {
+			logger.Log(
+				logger.LevelInfo,
+				map[string]string{
+					"message": fmt.Sprintf("duplicate cluster name: %s", current),
+				},
+				nil,
+				"cluster name already in use",
+			)
+
+			return false
+		}
+	}
+
+	return true
 }
 
 func (c *HeadlampConfig) addClusterSetupRoute(r *mux.Router) {

backend/cmd/headlamp_test.go
@@ -633,6 +633,58 @@ func TestHandleClusterAPI_XForwardedHost(t *testing.T) {
 	assert.Equal(t, "OK", rr.Body.String())
 }
 
+// Handle a cluster rename request.
+func handleClusterRenameRequest(
+	t *testing.T,
+	handler http.Handler,
+	tc struct {
+		name          string
+		clusterReq    RenameClusterRequest
+		expectedState int
+	},
+) {
+	var r *httptest.ResponseRecorder
+
+	var err error
+
+	if tc.clusterReq.Source == "kubeconfig" {
+		url := "/cluster/minikubetestnondynamic?clusterPathID=./headlamp_testdata/kubeconfig_rename:minikubetestnondynamic"
+		r, err = getResponseFromRestrictedEndpoint(handler, "PUT", url, tc.clusterReq)
+		require.NoError(t, err)
+		assert.Equal(t, tc.expectedState, r.Code)
+	} else {
+		url := "/cluster/minikubetest?clusterPathID=minikubetest"
+		r, err = getResponseFromRestrictedEndpoint(handler, "PUT", url, tc.clusterReq)
+		require.NoError(t, err)
+		assert.Equal(t, tc.expectedState, r.Code)
+	}
+}
+
+// Used to run the cluster rename tests.
+func runClusterRenameTests(
+	t *testing.T,
+	handler http.Handler,
+	tests []struct {
+		name          string
+		clusterReq    RenameClusterRequest
+		expectedState int
+	},
+) {
+	resetConfigByte, err := os.ReadFile("./headlamp_testdata/kubeconfig_rename")
+	require.NoError(t, err)
+
+	for _, tc := range tests {
+		handleClusterRenameRequest(t, handler, tc)
+	}
+
+	// this is a reset line that will reset the kubeconfig file to its original state
+	// without it the tests will fail as it makes changes to the kubeconfig
+	err = os.WriteFile("./headlamp_testdata/kubeconfig_rename", resetConfigByte, 0o600)
+	require.NoError(t, err)
+}
+
+// Test the cluster rename functionality.
+// note: needed to split into multiple parts for linter.
 func TestRenameCluster(t *testing.T) {
 	kubeConfigByte, err := os.ReadFile("./headlamp_testdata/kubeconfig")
 	require.NoError(t, err)
@@ -658,10 +710,7 @@ func TestRenameCluster(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	clusters := c.getClusters()
-
 	assert.Equal(t, http.StatusCreated, r.Code)
-	assert.Equal(t, 2, len(clusters))
 
 	tests := []struct {
 		name          string
@@ -669,29 +718,34 @@ func TestRenameCluster(t *testing.T) {
 		expectedState int
 	}{
 		{
+			name: "stateless",
+			clusterReq: RenameClusterRequest{
+				NewClusterName: "minikubetestworksnew",
+				Stateless:      true,
+			},
+			expectedState: http.StatusCreated,
+		},
+		{
 			name: "passStatefull",
 			clusterReq: RenameClusterRequest{
-				NewClusterName: "minikubetestworks",
+				NewClusterName: "minikubetestworkskubeconfig",
 				Stateless:      false,
 				Source:         "kubeconfig",
 			},
 			expectedState: http.StatusCreated,
 		},
-		{
-			name: "stateless",
-			clusterReq: RenameClusterRequest{
-				NewClusterName: "minikubetestworks",
-				Stateless:      true,
-			},
-			expectedState: http.StatusCreated,
-		},
 	}
 
-	for _, tc := range tests {
-		r, err = getResponseFromRestrictedEndpoint(handler, "PUT", "/cluster/minikubetest", tc.clusterReq)
-		require.NoError(t, err)
-		assert.Equal(t, tc.expectedState, r.Code)
-	}
+	runClusterRenameTests(t, handler, tests)
+
+	remErr := c.kubeConfigStore.RemoveContext("minikubetest")
+	require.NoError(t, remErr, "Failed to remove context: minikubetest")
+
+	remErrNonDy := c.kubeConfigStore.RemoveContext("minikubetestworkskubeconfig")
+	require.NoError(t, remErrNonDy, "Failed to remove context: minikubetestworkskubeconfig")
+
+	clusters := c.getClusters()
+	assert.Equal(t, 2, len(clusters))
 }
 
 func TestFileExists(t *testing.T) {

vyncent-t/headlamp-workspace

backend/cmd/headlamp.go
@@ -1508,9 +1508,15 @@ func (c *HeadlampConfig) deleteCluster(w http.ResponseWriter, r *http.Request) {
 	c.getConfig(w, r)
 }
 
-// Get path of kubeconfig from source.
+// Gets the path of the kubeconfig we load headlamp with from source.
+// Note for non dynamic clusters:
+//   - This will always give the kubeconfig we loaded headlamp with, but if more than one,
+//     it's not clear this will yield the correct value.
 func (c *HeadlampConfig) getKubeConfigPath(source string) (string, error) {
-	if source == "kubeconfig" {
+	// note: we need to have it in a const for the linting to pass
+	const sourceKubeconfig = "kubeconfig"
+
+	if source == sourceKubeconfig {
 		return c.kubeConfigPath, nil
 	}
 
@@ -1598,32 +1604,65 @@ func (c *HeadlampConfig) updateCustomContextToCache(config *api.Config, clusterN
 }
 
 // getPathAndLoadKubeconfig gets the path of the kubeconfig file and loads it.
-func (c *HeadlampConfig) getPathAndLoadKubeconfig(source, clusterName string) (string, *api.Config, error) {
-	// Get path of kubeconfig from source
-	path, err := c.getKubeConfigPath(source)
-	if err != nil {
-		logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
-			err, "getting kubeconfig file")
+// Note: the loadpath param is the direct path of the original kubeconfig if the cluster source is "kubeconfig".
+func (c *HeadlampConfig) getPathAndLoadKubeconfig(
+	source,
+	clusterName string,
+	loadPath string,
+) (string, *api.Config, error) {
+	var usePath string
 
-		return "", nil, err
+	var kubeConfig *api.Config
+
+	const sourceKubeconfig = "kubeconfig"
+
+	if source == sourceKubeconfig {
+		// If working from non dynamic cluster, we need to load the direct path of the original kubeconfig,
+		// not the headlamp default loaded kubeconfig.
+		// This handles uses where we have multiple kubeconfigs that we wish to use in headlamp
+		config, err := clientcmd.LoadFromFile(loadPath)
+		if err != nil {
+			logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+				err, "loading kubeconfig file")
+
+			return "", nil, err
+		}
+
+		usePath = loadPath
+		kubeConfig = config
+	} else {
+		// else we are working with a non dynamic cluster
+		// and the rename process is handled with the kubeconfig file loaded by headlamp
+		path, err := c.getKubeConfigPath(source)
+		if err != nil {
+			logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+				err, "getting kubeconfig file")
+
+			return "", nil, err
+		}
+
+		config, err := clientcmd.LoadFromFile(path)
+		if err != nil {
+			logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+				err, "loading kubeconfig file")
+
+			return "", nil, err
+		}
+
+		usePath = path
+		kubeConfig = config
 	}
 
-	// Load kubeconfig file
-	config, err := clientcmd.LoadFromFile(path)
-	if err != nil {
-		logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
-			err, "loading kubeconfig file")
-
-		return "", nil, err
-	}
-
-	return path, config, nil
+	return usePath, kubeConfig, nil
 }
 
 // Handler for renaming a cluster.
 func (c *HeadlampConfig) renameCluster(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	clusterName := vars["name"]
+	parts := strings.SplitN(r.URL.Query().Get("ClusterID"), ":", 2)
+	configPath := parts[0]
+
 	// Parse request body.
 	var reqBody RenameClusterRequest
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
@@ -1641,34 +1680,28 @@ func (c *HeadlampConfig) renameCluster(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var loadPath string
+	if reqBody.Source == "kubeconfig" {
+		loadPath = configPath
+	}
+
 	// Get path of kubeconfig from source
-	path, config, err := c.getPathAndLoadKubeconfig(reqBody.Source, clusterName)
+	path, config, err := c.getPathAndLoadKubeconfig(reqBody.Source, clusterName, loadPath)
 	if err != nil {
 		http.Error(w, "getting kubeconfig file", http.StatusInternalServerError)
 		return
 	}
 
-	// Find the context with the given cluster name
-	contextName := clusterName
+	isUnique := checkUniqueName(config, clusterName, reqBody.NewClusterName)
+	if !isUnique {
+		http.Error(w, "custom name already in use", http.StatusBadRequest)
+		logger.Log(logger.LevelError, map[string]string{"cluster": clusterName},
+			err, "cluster name already exists in the kubeconfig")
 
-	// Iterate over the contexts to find the context with the given cluster name
-	for k, v := range config.Contexts {
-		info := v.Extensions["headlamp_info"]
-		if info != nil {
-			customObj, err := MarshalCustomObject(info, contextName)
-			if err != nil {
-				logger.Log(logger.LevelError, map[string]string{"cluster": contextName},
-					err, "marshaling custom object")
-
-				return
-			}
-
-			// Check if the CustomName field matches the cluster name
-			if customObj.CustomName != "" && customObj.CustomName == clusterName {
-				contextName = k
-			}
-		}
+		return
 	}
+
+	contextName := findMatchingContextName(config, clusterName)
 
 	if err := customNameToExtenstions(config, contextName, reqBody.NewClusterName, path); err != nil {
 		http.Error(w, "writing custom extension to kubeconfig", http.StatusInternalServerError)
@@ -1682,6 +1715,76 @@ func (c *HeadlampConfig) renameCluster(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	c.getConfig(w, r)
+}
+
+// findMatchingContextName checks all contexts, returning the key for whichever
+// has a matching customObj.CustomName, if any.
+func findMatchingContextName(config *api.Config, clusterName string) string {
+	contextName := clusterName
+
+	for k, v := range config.Contexts {
+		info := v.Extensions["headlamp_info"]
+		if info != nil {
+			customObj, err := MarshalCustomObject(info, contextName)
+			if err != nil {
+				logger.Log(logger.LevelError, map[string]string{"cluster": contextName},
+					err, "marshaling custom object")
+				continue
+			}
+
+			if customObj.CustomName == clusterName && customObj.CustomName != "" {
+				contextName = k
+			}
+		}
+	}
+
+	return contextName
+}
+
+// checkUniqueName returns false if 'newName' is already in 'names' otherwise returns true.
+// used for checking cluster names.
+func checkUniqueName(config *api.Config, contextName string, newName string) bool {
+	contextNames := make([]string, 0, len(config.Contexts))
+
+	for name := range config.Contexts {
+		contextNames = append(contextNames, name)
+		logger.Log(logger.LevelInfo, map[string]string{"cluster added": name},
+			nil, "context name")
+	}
+
+	// Iterate over the contexts and add the custom names
+	for _, y := range config.Contexts {
+		info := y.Extensions["headlamp_info"]
+		if info != nil {
+			customObj, err := MarshalCustomObject(info, contextName)
+			if err != nil {
+				logger.Log(logger.LevelError, map[string]string{"cluster": contextName},
+					err, "marshaling custom object")
+			}
+
+			// add custom name if it is not empty
+			if customObj.CustomName != "" {
+				contextNames = append(contextNames, customObj.CustomName)
+			}
+		}
+	}
+
+	for _, current := range contextNames {
+		if current == newName {
+			logger.Log(
+				logger.LevelInfo,
+				map[string]string{
+					"message": fmt.Sprintf("duplicate cluster name: %s", current),
+				},
+				nil,
+				"cluster name already in use",
+			)
+
+			return false
+		}
+	}
+
+	return true
 }
 
 func (c *HeadlampConfig) addClusterSetupRoute(r *mux.Router) {

backend/cmd/headlamp_test.go
@@ -633,6 +633,58 @@ func TestHandleClusterAPI_XForwardedHost(t *testing.T) {
 	assert.Equal(t, "OK", rr.Body.String())
 }
 
+// Handle a cluster rename request.
+func handleClusterRenameRequest(
+	t *testing.T,
+	handler http.Handler,
+	tc struct {
+		name          string
+		clusterReq    RenameClusterRequest
+		expectedState int
+	},
+) {
+	var r *httptest.ResponseRecorder
+
+	var err error
+
+	if tc.clusterReq.Source == "kubeconfig" {
+		url := "/cluster/minikubetestnondynamic?ClusterID=./headlamp_testdata/kubeconfig_rename:minikubetestnondynamic"
+		r, err = getResponseFromRestrictedEndpoint(handler, "PUT", url, tc.clusterReq)
+		require.NoError(t, err)
+		assert.Equal(t, tc.expectedState, r.Code)
+	} else {
+		url := "/cluster/minikubetest?ClusterID=minikubetest"
+		r, err = getResponseFromRestrictedEndpoint(handler, "PUT", url, tc.clusterReq)
+		require.NoError(t, err)
+		assert.Equal(t, tc.expectedState, r.Code)
+	}
+}
+
+// Used to run the cluster rename tests.
+func runClusterRenameTests(
+	t *testing.T,
+	handler http.Handler,
+	tests []struct {
+		name          string
+		clusterReq    RenameClusterRequest
+		expectedState int
+	},
+) {
+	resetConfigByte, err := os.ReadFile("./headlamp_testdata/kubeconfig_rename")
+	require.NoError(t, err)
+
+	for _, tc := range tests {
+		handleClusterRenameRequest(t, handler, tc)
+	}
+
+	// this is a reset line that will reset the kubeconfig file to its original state
+	// without it the tests will fail as it makes changes to the kubeconfig
+	err = os.WriteFile("./headlamp_testdata/kubeconfig_rename", resetConfigByte, 0o600)
+	require.NoError(t, err)
+}
+
+// Test the cluster rename functionality.
+// note: needed to split into multiple parts for linter.
 func TestRenameCluster(t *testing.T) {
 	kubeConfigByte, err := os.ReadFile("./headlamp_testdata/kubeconfig")
 	require.NoError(t, err)
@@ -658,10 +710,7 @@ func TestRenameCluster(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	clusters := c.getClusters()
-
 	assert.Equal(t, http.StatusCreated, r.Code)
-	assert.Equal(t, 2, len(clusters))
 
 	tests := []struct {
 		name          string
@@ -669,29 +718,34 @@ func TestRenameCluster(t *testing.T) {
 		expectedState int
 	}{
 		{
+			name: "stateless",
+			clusterReq: RenameClusterRequest{
+				NewClusterName: "minikubetestworksnew",
+				Stateless:      true,
+			},
+			expectedState: http.StatusCreated,
+		},
+		{
 			name: "passStatefull",
 			clusterReq: RenameClusterRequest{
-				NewClusterName: "minikubetestworks",
+				NewClusterName: "minikubetestworkskubeconfig",
 				Stateless:      false,
 				Source:         "kubeconfig",
 			},
 			expectedState: http.StatusCreated,
 		},
-		{
-			name: "stateless",
-			clusterReq: RenameClusterRequest{
-				NewClusterName: "minikubetestworks",
-				Stateless:      true,
-			},
-			expectedState: http.StatusCreated,
-		},
 	}
 
-	for _, tc := range tests {
-		r, err = getResponseFromRestrictedEndpoint(handler, "PUT", "/cluster/minikubetest", tc.clusterReq)
-		require.NoError(t, err)
-		assert.Equal(t, tc.expectedState, r.Code)
-	}
+	runClusterRenameTests(t, handler, tests)
+
+	remErr := c.kubeConfigStore.RemoveContext("minikubetest")
+	require.NoError(t, remErr, "Failed to remove context: minikubetest")
+
+	remErrNonDy := c.kubeConfigStore.RemoveContext("minikubetestworkskubeconfig")
+	require.NoError(t, remErrNonDy, "Failed to remove context: minikubetestworkskubeconfig")
+
+	clusters := c.getClusters()
+	assert.Equal(t, 2, len(clusters))
 }
 
 func TestFileExists(t *testing.T) {